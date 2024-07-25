7dfa034eb73ef0461fa570dd9405f985c1909231
NickelTing <125737200+NickelTing@users.noreply.github.com>
2024-07-26 11:31:43 +1200

    begin backend

---
+++ backend/Models/Burger.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public class Burger
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public boolean IsVegan { get; set; }
+    }
+}
