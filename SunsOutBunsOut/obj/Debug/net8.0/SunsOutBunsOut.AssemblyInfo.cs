a929de58cd9e4bf8cf9067524fb91515af104623
NickelTing <125737200+NickelTing@users.noreply.github.com>
2024-07-26 13:56:20 +1200

    add root

--- SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
+++ SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3f42099ce89bede583ef654bc815eca7eaa1ab3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e09895986923f5f3b3c6723354ee47773a5dd77")]
 [assembly: System.Reflection.AssemblyProductAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
