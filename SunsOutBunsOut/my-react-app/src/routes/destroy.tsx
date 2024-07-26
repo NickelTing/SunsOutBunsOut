92fb3d03fcc2309b5d649fc432d15ccf1a1465c9
NickelTing <125737200+NickelTing@users.noreply.github.com>
2024-07-27 10:55:05 +1200

    delete my-react-app

--- SunsOutBunsOut/my-react-app/src/routes/destroy.tsx
+++
@@ -1,8 +0,0 @@
-import { ActionFunctionArgs, redirect,  } from "react-router-dom";
-import { deleteContact } from "../contacts.ts";
-
-export async function action({ params }: ActionFunctionArgs) {
-    throw new Error("oh dang!");
-    await deleteContact(params.contactId);
-    return redirect("/");
-}
--- SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
+++ SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba90cbb374abab4206698145eb9a54b612dcfd6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+942046b040e49d3de0ccf4616a19afb64c27b8eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
