4e9d8bb292c5a851d2db01d97a324b2fc1b74b89
NickelTing <125737200+NickelTing@users.noreply.github.com>
2024-07-26 14:42:37 +1200

    changes to frontend

--- SunsOutBunsOut/my-react-app/src/main.tsx
+++ SunsOutBunsOut/my-react-app/src/main.tsx
@@ -5,13 +5,23 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import Root from './routes/root.tsx';
+import Root from './routes/root';
+import ErrorPage from './error-page';
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: < Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
   },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
+++ SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e09895986923f5f3b3c6723354ee47773a5dd77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a929de58cd9e4bf8cf9067524fb91515af104623")]
 [assembly: System.Reflection.AssemblyProductAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
