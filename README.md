302e80e03bc87790e255424dcf8c65f1ab92a3a5
NickelTing <125737200+NickelTing@users.noreply.github.com>
2024-07-29 14:49:07 +1200

    update readme

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ Install the packages by running the commands below:
 - npm install react-router-dom
 - npm install localforage match-sorter sort-by
 
+# Running my-react-app
+Locate the my-react-app folder by running cd SunsOutBunsOut/my-react-app, or right clicking the my-react-app folder and select Open in Integrated Folder. Run npm run dev to run the project.
+
 # Database
 The data are retrieved and stored in a local MySQL database instance (I tried to use Azure SQL but I did not have a subscription to deploy one). A SQL script is included in this directory which you can use to create database and table and import sample data.
--- SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
+++ SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a718a0d71bc9106d52791d111841842f306418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb1f72d0adb046b6225674e5c0ed1f2a32e2b73")]
 [assembly: System.Reflection.AssemblyProductAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
