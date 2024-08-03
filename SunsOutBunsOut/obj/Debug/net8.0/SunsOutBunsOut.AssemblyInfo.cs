946369e12026a6693704d3d04cd57e386079ab70
NickelTing <125737200+NickelTing@users.noreply.github.com>
2024-08-03 12:04:21 +1200

    some changes

--- SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
+++ SunsOutBunsOut/obj/Debug/net8.0/SunsOutBunsOut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5aeec22bdc5dd52c8fd010359d7f773b3c0e51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+331d2b12299524038288cad094192634f55742fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SunsOutBunsOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
